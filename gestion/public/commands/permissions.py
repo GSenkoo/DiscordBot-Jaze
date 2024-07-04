6987b2db1ac673acd4271330389ca4e9bf5d8071
GSenkoo <senkoo.g4l@gmail.com>
2024-07-04 22:50:19 +0200

    Pour la commande +perms : Ajout de la possibilitée d'ajouter/de retirer des rôles autorisés & ajout de la possibilitée d'ajouter des utilisateurs, le reste est à faire plus tards. PS : La commande +perms est toujours en développement

--- gestion/public/commands/permissions.py
+++ gestion/public/commands/permissions.py
@@ -1,10 +1,26 @@
 import discord
 import textwrap
+import json
 from datetime import datetime
 from discord.ext import commands
 from utils.PermissionsManager import PermissionsManager
 from utils.Paginator import PaginatorCreator
+from utils.Database import Database
 
+
+class MyViewClass(discord.ui.View):
+    async def on_timeout(self):
+        if self.to_components != self.message.components:
+            return
+        if self.message:
+            try: await self.message.edit(view = None)
+            except: pass
+
+custom_names = {
+    "0": "Public",
+    "10": "Owner",
+    "11": "Propriéataire"
+}
 
 class Gestion_des_Permissions(commands.Cog):
     def __init__(self, bot):
@@ -16,7 +32,7 @@ class Gestion_des_Permissions(commands.Cog):
     async def guideperms(self, ctx):
         bot = self.bot
 
-        class PermGuideMenu(discord.ui.View):
+        class PermGuideMenu(MyViewClass):
             async def on_timeout(self):
                 try: await self.message.edit(view = None)
                 except: pass
@@ -94,11 +110,13 @@ class Gestion_des_Permissions(commands.Cog):
     @commands.command(description = "Voir les permissions hiérarchiques")
     @commands.guild_only()
     async def perms(self, ctx):
-        class ConfigPerms(discord.ui.View):
+        bot = self.bot
+
+        class ConfigPerms(MyViewClass):
             @discord.ui.select(
                 placeholder = "Choisir une permission",
                 options = [
-                    discord.SelectOption(label = f"Perm{i}", emoji = "🔒", value = str(i)) for i in range(1, 10)
+                    discord.SelectOption(label = f"Perm{i}", emoji = "🗝", value = str(i)) for i in range(12) if not custom_names.get(str(i), None)
                 ]
             )
             async def select_callback(self, select, interaction):
@@ -106,17 +124,241 @@ class Gestion_des_Permissions(commands.Cog):
                     await interaction.response.send_message("> Vous n'êtes pas autorisés à intéragir avec ceci.", ephemeral = True)
                     return
                 
+                original_permission = select.values[0]
 
+                async def get_permission_embed():
+                    db = Database()
+                    await db.connect()
+                    permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = ctx.guild.id))
+                    await db.disconnect()
+
+                    perm_authorzation_data = permissions_data["authorizations"][select.values[0]]
+                    perm_commands = [command for command, permission in permissions_data["commands"].items() if permission == select.values[0]]
+
+                    embed = discord.Embed(
+                        title = custom_names.get(select.values[0], f"Permission n°{select.values[0]}"),
+                        color = await bot.get_theme(ctx.guild.id),
+                        description = textwrap.dedent(f"""
+                            *Chaque rôle, utilisateur ou permission de serveur doit appartenir à une seule permission hiérarchique. Ainsi, si vous ajoutez un élément déjà présent dans une autre permission hiérarchique, il sera automatiquement retiré de cette dernière.*
+                            ### Informations sur la permission
+                            *Commandes* : **{len(perm_commands)}**
+                            *Rôles* : **{len(perm_authorzation_data['roles'])}/15**
+                            *Utilisateur spécifiques* : **{len(perm_authorzation_data['users'])}/15**
+                            *Permissions de serveur* : **{len(perm_authorzation_data['guildpermissions'])}/15**
+                        """),
+
+                    ).add_field(
+                        name = "Rôles autorisés",
+                        value = "\n".join([f"<@&{role_id}>" for role_id in perm_authorzation_data["roles"]]) if perm_authorzation_data["roles"] else "*Aucun rôle*"
+                    ).add_field(
+                        name ="Utilisateurs autorisés",
+                        value = "\n".join([f"<@{user_id}>" for user_id in perm_authorzation_data["users"]]) if perm_authorzation_data["users"] else "*Aucun utilisateur*"
+                    ).add_field(
+                        name = "Permissions autorisés",
+                        value = "\n".join(perm_authorzation_data["guildpermissions"]) if perm_authorzation_data["guildpermissions"] else "*Aucune permission autorisé*"
+                    )
+
+                    return embed
+                
+                class EditPerm(MyViewClass):
+                    @discord.ui.select(
+                        placeholder = "Modifier le select menu",
+                        options = [
+                            discord.SelectOption(label = "Ajouter des rôles", emoji = "🎭", value = "add_roles"),
+                            discord.SelectOption(label = "Retirer des rôles", emoji = "🎭", value = "remove_roles"),
+                            discord.SelectOption(label = "Ajouter des utilisateurs", emoji = "👥", value = "add_users"),
+                            discord.SelectOption(label = "Retirer des utilisateurs", emoji = "👥", value = "remove_users"),
+                            discord.SelectOption(label = "Gérer les permissions", emoji = "🗝", value = "manage_guild")
+                        ]
+                    )
+                    async def select_callback(self, select, interaction):
+                        if interaction.user != ctx.author:
+                            await interaction.response.send_message("> Vous n'êtes pas autorisés à intéragir avec ceci.", ephemeral = True)
+                            return
+
+                        db = Database()
+                        await db.connect()
+                        permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = interaction.guild.id))
+                        permission_data = permissions_data["authorizations"][original_permission]
+                        await db.disconnect()
+
+                        previous_view = self
+                        if select.values[0] == "add_roles":
+                            if len(permission_data["roles"]) >= 15:
+                                await interaction.response.send_message("> Vous ne pouvez pas ajouter plus de 15 rôles autorisés.", ephemeral = True)
+                                return
+                            
+                            await interaction.response.defer()
+                            
+                            class AddRole(MyViewClass):
+                                @discord.ui.select(
+                                    placeholder = "Choisir des rôles",
+                                    select_type = discord.ComponentType.role_select,
+                                    max_values = 15 - len(permission_data["roles"])
+                                )
+                                async def add_role_callback(self, select, interaction):
+                                    if interaction.user != ctx.author:
+                                        await interaction.response.send_message("> Vous n'êtes pas autorisés à intéragir avec ceci.", ephemeral = True)
+                                        return
+                                    
+                                    db = Database()
+                                    await db.connect()
+
+                                    permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = interaction.guild.id))
+                                    permission_data = permissions_data["authorizations"][original_permission]
+
+                                    for role in select.values:
+                                        if (role.id in permission_data["roles"]) or (len(permission_data["roles"]) >= 15):
+                                            continue
+                                        for perm, perm_data in permissions_data["authorizations"].items(): # Faire en sorte à ce que le rôle ne soit nul par ailleurs.
+                                            if role.id in perm_data["roles"]:
+                                                permissions_data["authorizations"][perm]["roles"].remove(role.id)
+                                        permission_data["roles"].append(role.id)
+
+                                    permissions_data["authorizations"][original_permission] = permission_data
+                                    await db.set_data("guild", "perms_hierarchic", json.dumps(permissions_data), guild_id = interaction.guild.id)
+                                    await interaction.response.defer()
+                                    await interaction.message.edit(embed = await get_permission_embed(), view = previous_view)
+
+                                    await db.disconnect()
+
+
+                                @discord.ui.button(label = "Choisissez des rôles à ajouter", row = 1, style = discord.ButtonStyle.primary, disabled = True)
+                                async def callback(self, button, interaction):
+                                    pass
+
+                            await interaction.message.edit(view = AddRole())
+
+                        if select.values[0] == "remove_roles":
+                            if not permission_data["roles"]:
+                                await interaction.response.send_message(f"> Il n'y pour le moment aucun rôle à retirer pour la **{'Perm' + original_permission}**.", ephemeral = True)
+                                return
+                            
+                            await interaction.response.defer()
+                            
+                            roles_data = {}
+                            guild_roles = await interaction.guild.fetch_roles()
+                            guild_roles_ids = [role.id for role in guild_roles]
+                            
+                            for role_id in permission_data["roles"]:
+                                if role_id not in guild_roles_ids:
+                                    roles_data[str(role_id)] = "@RôleIntrouvable"
+                                    continue
+                                roles_data[str(role_id)] = "@" + guild_roles[guild_roles_ids.index(role_id)].name
+
+                            
+                            class RemoveRole(MyViewClass):
+                                @discord.ui.select(
+                                    placeholder = "Choisir des rôles",
+                                    max_values = len(roles_data),
+                                    options = [
+                                        discord.SelectOption(label = role_name, value = role_id, description = f"Identifiant : {role_id}") for role_id, role_name in roles_data.items()
+                                    ]
+                                )
+                                async def remove_role_callback(self, select, interaction):
+                                    if interaction.user != ctx.author:
+                                        await interaction.response.send_message("> Vous n'êtes pas autorisés à intéragir avec ceci.", ephemeral = True)
+                                        return
+                                    
+                                    db = Database()
+                                    await db.connect()
+
+                                    permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = interaction.guild.id))
+                                    permission_data = permissions_data["authorizations"][original_permission]
+
+                                    for role_id in select.values:
+                                        if int(role_id) not in permission_data["roles"]:
+                                            continue
+                                        permission_data["roles"].remove(int(role_id))
+                                    
+                                    permissions_data["authorizations"][original_permission] = permission_data
+                                    await db.set_data("guild", "perms_hierarchic", json.dumps(permissions_data), guild_id =  interaction.guild.id)
+                                    await interaction.response.defer()
+                                    await interaction.message.edit(embed = await get_permission_embed(), view = previous_view)
+                                    await db.disconnect()
+
+                                @discord.ui.button(label = "Choisissez des rôles à supprimer", disabled = True, style = discord.ButtonStyle.primary)
+                                async def callback(self, button, interaction):
+                                    pass
+
+                            await interaction.message.edit(view = RemoveRole())
+
+                        if select.values[0] == "add_users":
+                            if len(permission_data["users"]) >= 15:
+                                await interaction.response.send_message("> Vous ne pouvez pas ajouter plus de 15 utilisateurs.", ephemeral = True)
+                                return
+                            
+                            await interaction.response.defer()
+                            
+                            class AddUsers(MyViewClass):
+                                @discord.ui.select(
+                                    placeholder = "Choisir un utilisateur",
+                                    select_type = discord.ComponentType.user_select,
+                                    max_values = 15 - len(permission_data["users"])
+                                )
+                                async def add_users_callback(self, select, interaction):
+                                    if interaction.user != ctx.author:
+                                        await interaction.response.send_message("> Vous n'êtes pas autorisés à intéragir avec ceci.", ephemeral = True)
+                                        return
+                                    
+                                    db = Database()
+                                    await db.connect()
+
+                                    permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = interaction.guild.id))
+                                    permission_data = permissions_data["authorizations"][original_permission]
+
+                                    for user in select.values:
+                                        if (user.id in permission_data["users"]) or (len(permission_data["users"]) >= 15):
+                                            continue
+                                        for perm, perm_data in permissions_data["authorizations"].items(): # Faire en sorte à ce que l'utilisateur ne soit nul par ailleurs.
+                                            if user.id in perm_data["users"]:
+                                                permissions_data["authorizations"][perm]["users"].remove(user.id)
+                                        permission_data["users"].append(user.id)
+
+                                    permissions_data["authorizations"][original_permission] = permission_data
+                                    await db.set_data("guild", "perms_hierarchic", json.dumps(permissions_data), guild_id =  interaction.guild.id)
+                                    await interaction.response.defer()
+                                    await interaction.message.edit(embed = await get_permission_embed(), view = previous_view)
+                                    await db.disconnect()
+
+
+                                @discord.ui.button(label = "Choisissez des utilisateurs à ajouter", disabled = True, style = discord.ButtonStyle.primary)
+                                async def callback(self, button, interaction):
+                                    pass
+
+                            await interaction.message.edit(view = AddUsers())
+
+                        # FAIRE remove_users et manage_guild
+
+                await interaction.response.defer()
+                for option in select.options:
+                    option.default = option.value == select.values[0]
+
+                await interaction.message.edit(embed = await get_permission_embed(), view = EditPerm())
+
+
+                
+        db = Database()
+        await db.connect()
+        permissions_data = json.loads(await db.get_data("guild", "perms_hierarchic", guild_id = ctx.guild.id))
+        await db.disconnect()
+
+        perms = {str(k):0 for k in range(12)}
+        for name, perm in permissions_data["commands"].items():
+            perms[perm] += 1
+        perms = [" - **" + custom_names.get(perm, f"Perm{perm}") + "**" + f" ({cmds_count})" for perm, cmds_count in perms.items() if cmds_count != 0]
         embed = discord.Embed(
             title = "Permissions hiérarchiques",
+            color = await self.bot.get_theme(ctx.guild.id),
             description = textwrap.dedent(f"""
                 *Vous pouvez voir et modifier vos permsisions via le menu ci-dessous*
                 *Pour voir les commandes par permissions, utilisez la commande `{await self.bot.get_prefix(ctx.message)}helpall`.*
 
-                **__Vos permissions configurés__**
-
-            """)
+                - **__Permissions possédant des commandes__**
+            """) + '\n'.join(perms)
         )
+
+        await ctx.send(embed = embed, view = ConfigPerms())
 
 
     @commands.command(description = "Voir vos commandes par permissions hiérarchiques")
@@ -126,11 +368,6 @@ class Gestion_des_Permissions(commands.Cog):
         paginator_creator = PaginatorCreator()
 
         prefix = await self.bot.get_prefix(ctx.message)
-        custom_names = {
-            "0": "Public",
-            "10": "Owner",
-            "11": "Propriéataire"
-        }
         descriptions = []
         for index in range(12):
             commands = await perms_manager.get_perm_commands(ctx.guild.id, index)
